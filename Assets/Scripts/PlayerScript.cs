afc15a099a59cc33543735fe80f047422b2f4ab4
Dahlia-sung <108792798+Dahlia-sung@users.noreply.github.com>
2022-10-20 09:29:37 +0900

    1020 morning
    창우가 아침에 봐줌

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,7 +6,7 @@ using Photon.Realtime;
 using TMPro;
 using UnityEngine.UI;
 
-public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
+public class PlayerScript : MonoBehaviourPunCallbacks
 {
 
     [SerializeField]
@@ -24,7 +24,7 @@ public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
 
     private GameObject JumpGO;
     private Button JumpBtn;
-    bool isGround;
+    bool isGround = true;
     Vector3 curPos;
     bool gamestart = false;
 
@@ -51,23 +51,23 @@ public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
                 AN.SetBool("idle", false);
             }
 
-            if (PV.IsMine)
-            {
-                Debug.DrawRay(transform.position, new Vector3(0, -1f, 0), new Color(0, 1, 0));
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, LayerMask.GetMask("Ground"));
-                if (hit.collider == null)
-                {
-                    isGround = false;
-                    AN.SetBool("jump", true);
-                }
-                else
-                {
-                    isGround = true;
-                    AN.SetBool("jump", false);
-                }
-            }
-            else if ((transform.position - curPos).sqrMagnitude >= 100) transform.position = curPos;
-            else transform.position = Vector3.Lerp(transform.position, curPos, Time.deltaTime * 10);
+            //if (PV.IsMine)
+            //{
+            //    Debug.DrawRay(transform.position, new Vector3(0, -0.01f, 0), new Color(0, 1, 0));
+            //    //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, LayerMask.GetMask("Ground"));
+            //    //if (hit.collider == null)
+            //    //{
+            //    //    isGround = true;
+            //    //    AN.SetBool("jump", fla);
+            //    //}
+            //    //else
+            //    //{
+            //    //    isGround = true;
+            //    //    AN.SetBool("jump", false);
+            //    //}
+            //}
+            //else if ((transform.position - curPos).sqrMagnitude >= 100) transform.position = curPos;
+            //else transform.position = Vector3.Lerp(transform.position, curPos, Time.deltaTime * 10);
         }
     }
 
@@ -75,27 +75,46 @@ public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
     // 점프 버튼에 할당
     public void Jump()
     {
-        if(isGround == true)
+        Debug.Log(isGround);
+        if (PV.IsMine && isGround == true)
         {
-            PV.RPC("JumpRPC", RpcTarget.All);
+            Debug.Log("aa");
+            RB.AddForce(Vector2.up * 700, ForceMode2D.Force);
+            //PV.RPC("JumpRPC", RpcTarget.All);
         }
     }
     [PunRPC]
     void JumpRPC()
     {
-        RB.AddForce(Vector2.up * 700);
+        RB.AddForce(Vector2.up * 700, ForceMode2D.Force);
     }
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //{
+    //    if(stream.IsWriting)
+    //    {
+    //        stream.SendNext(transform.position);
+    //        stream.SendNext(HealthImage.fillAmount);
+    //    }
+    //    else
+    //    {
+    //        curPos = (Vector3)stream.ReceiveNext();
+    //        HealthImage.fillAmount = (float)stream.ReceiveNext();
+    //    }
+    //}
+
+    void OnCollisionEnter(Collision collision)
     {
-        if(stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-            stream.SendNext(HealthImage.fillAmount);
+        if (collision.gameObject.layer == LayerMask.GetMask("Ground")) {
+            isGround = true;
+            AN.SetBool("jump", false);
         }
-        else
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.GetMask("Ground"))
         {
-            curPos = (Vector3)stream.ReceiveNext();
-            HealthImage.fillAmount = (float)stream.ReceiveNext();
+            isGround = false;
+            AN.SetBool("jump", true);
         }
     }
 }
